3d8783336c10bb41e4daffbe2da9dfc80aedd509
kaabdou000-cmyk <kaabdou000@gmail.com>
2026-02-18 08:47:46 +0100

    nmbreimpaire

--- github.c
+++ github.c
@@ -6,6 +6,7 @@ int main (){
             printf("saisir n entiers : ");
             scanf("%d" ,&n);
     }while(n<0);
+    cpt=0;
     for(int i=0;i<n;i++){
         do{
             printf("saisir un entier negatif :");
@@ -14,11 +15,15 @@ int main (){
         if (x%2==0){
             som=som+x;
            cpt++;
+        }else
+        if(x%2!=0){
+            cpt+=1;
         }
     }
 
     moy = som/cpt;
     printf("la moyenne des nombres paire est :%f",moy);
+    printf("le nombre de nombre impaire est : %d",cpt);
 
 return 0 ;
 }
